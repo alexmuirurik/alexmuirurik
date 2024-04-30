2e79b258c39f29266e83b36abf42340b503b586f
alexmuirurik <content@alexmuiruri.com>
2024-04-30 03:58:45 -0400

    Updated the posts database to fetch data from wordpress

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,10 +3,10 @@ import SinglePostCard from '@/components/card/SinglePostCard'
 import PostCard from '@/components/card/PostCard'
 import { singlepost } from '@/utils/posts'
 
-const page = async ({params}: {params: {slug: string}}) => {
+const page = async ({params, searchParams}: {params: {slug: string}, searchParams?: { [key: string]: string | string[] | undefined } }) => {
+	const pgs = (searchParams?.page) ? Number(searchParams?.page) : 1 
 	const req	= await singlepost(params.slug)
 	const post	= await req.json()
-	console.log(post[0])
 	return (
 		<div className="row mx-0 my-2">
 			<div className="col md:w-9/12 px-0">
@@ -26,7 +26,7 @@ const page = async ({params}: {params: {slug: string}}) => {
 				</div>
 			</div>
 			<div className="col md:w-3/12">
-				<PostCard />
+				<PostCard page={pgs} />
 			</div>
 		</div>
 	)
