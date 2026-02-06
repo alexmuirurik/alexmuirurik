7c5368bd8754ec620651b04ded9cd7c854dae771
Alex Muiruri <content@alexmuiruri.com>
2026-02-06 05:27:16 +0300

    Added custom border colors

--- src/components/dashboard/articles.tsx
+++ src/components/dashboard/articles.tsx
@@ -4,7 +4,7 @@ const Articles = () => {
     return (
         <div className="bg-ghost-white dark:bg-card-dark w-full md:w-4/12 lg:w-3/12 rounded-md py-6 px-4">
             <h2 className="card-title">Articles</h2>
-            <p className="section-description text-sm">
+            <p className="section-description text-sm text-gray-500">
                 Sample Articles I&apos;ve written before
             </p>
             <div className="card">
--- src/components/dashboard/renderprojects.tsx
+++ src/components/dashboard/renderprojects.tsx
@@ -1,31 +1,39 @@
 'use client'
 
 import { siteMeta } from '@/src/lib/sitemeta'
-import Link from 'next/link'
 import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel'
 import Autoplay from 'embla-carousel-autoplay'
 import Image from 'next/image'
 
 const RenderProjects = () => {
     return (
-        <Carousel plugins={[Autoplay()]} >
+        <Carousel plugins={[Autoplay()]}>
             <CarouselContent className="gap-4 ps-4 ">
                 {siteMeta.recentWork.map((project) => (
                     <CarouselItem
                         key={project.name}
-                        className="basis-1/2 border border-neutral-600 rounded-md space-y-2 ps-0"
+                        className="basis-1/2 border border-gray-600 rounded-md space-y-2 ps-0"
                     >
-                        <div className="border-b border-neutral-600 p-2">
+                        <div className="border-b border-gray-600 p-2">
                             <Image
                                 className="!static rounded-md object-fill aspect-video"
                                 src={project.image}
                                 alt={project.name}
-                                fill 
+                                fill
                             />
                         </div>
                         <div className="py-2 px-4 space-y-1">
-                            <span className="text-sm text-gray-400 font-bold">{project.name}</span>
-                            <p className="text-xs text-gray-500">{project.description}</p>
+                            <span className="text-sm text-gray-400 font-bold">
+                                {project.name}
+                            </span>
+                            {project.description.map((description) => (
+                                <p
+                                    key={description}
+                                    className="text-xs text-gray-500"
+                                >
+                                    {description}
+                                </p>
+                            ))}
                         </div>
                     </CarouselItem>
                 ))}
